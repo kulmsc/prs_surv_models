433c492703151bd3e0afb560c8c6732c483c6c8a
Scott Kulm <kulmsc@nice.pbtech>
2022-03-12 09:08:54 -0500

    test

--- adjustment/run.sh
+++ adjustment/run.sh
@@ -1,13 +1,19 @@
 author=$1
 
-Rscript get_y_data.R $author
-python zero_enet_cox.py
-Rscript analysis.R $author
-mv static_pred.txt.gz res/${author}.enet_pred.gz
-mv coef.txt.gz res/${author}.coef.gz
-
+#Rscript get_y_data.R $author
+#python zero_enet_cox.py
+#Rscript analysis.R $author
+#mv static_pred.txt.gz res/${author}.enet_pred.gz
+#mv coef.txt.gz res/${author}.coef.gz
+#mv train_pred.txt.gz res/${author}.train_pred.gz
+#mv best_alpha.txt res/${author}.best_alpha.txt
 
 #go backwards
-#author=tsoi
-#cp res/${author}.enet_pred.gz static_pred.txt.gz
-#cp res/${author}.coef.gz coef.txt.gz
+Rscript get_y_data.R $author
+cp res/${author}.enet_pred.gz static_pred.txt.gz
+cp res/${author}.coef.gz coef.txt.gz
+cp res/${author}.train_pred.gz train_pred.txt.gz
+#Rscript analysis.R $author
+Rscript gof.R $author
+#Rscript  attach_pheno.R $author
+#rm coef.txt.gz static_pred.txt.gz train_pred.txt.gz
